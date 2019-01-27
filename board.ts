a754d02a7b987f630886978329cbe0c2a40196d9
Shasheen Ediriweera <shasheene@gmail.com>
2019-01-27 11:29:45 +1030

    Draw unicode chess piece characters

--- board.ts
+++ board.ts
@@ -30,6 +30,7 @@ class Board{
 		let temp_move_options : Coordinate[];
 		if (this.turn_state == TurnState.NO_PIECE_SELECTED) {
 			this.selected_piece_coords = checker_index;
+
 			temp_move_options = [{x_offset: 3, y_offset: 5}, {x_offset: 7, y_offset: 2}]
 			this.drawBoard(this.selected_piece_coords, temp_move_options);
 		} else if (this.turn_state == TurnState.PIECE_SELECTED) {
@@ -46,6 +47,31 @@ class Board{
 		}
 	}
 
+	convertStringToPiece(col: string, piece: string): string {
+		// From UTF-16 decimal index 9812, there are a set of white pieces, which are an outline.
+		// However, returning the internally filled black code points (from 9818) can be filled to
+		// a color and look much much better.
+		if (piece == "k") {
+			// King
+			return String.fromCharCode(9818);
+		} else if (piece == "q") {
+			// Queen
+			return String.fromCharCode(9819);
+		} else if (piece == "r") {
+			// Rook
+			return String.fromCharCode(9820);
+		} else if (piece == "b") {
+			// Bishop
+			return String.fromCharCode(9821);
+		} else if (piece == "h") {
+			// Knight
+			return String.fromCharCode(9822);
+		} else if (piece == "p") {
+			// Pawn
+			return String.fromCharCode(9823);
+		}
+	}	
+
 	/**
 	 * Draws board checker pattern, and populates game pieces.
 	 *
@@ -53,13 +79,17 @@ class Board{
 	 * @param possible_moves Array of moves that @param selected_checker can make
 	 */
 	drawBoard(selected_checker?: Coordinate, possible_moves?: Coordinate[]) {
+		let game_state_message_string = '{"message_type": "GET_GAME_STATE", "payload": {"board": [["br", "bh", "bb", "bq", "bk", "bb", "bh", "br"], ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"], ["_", "_", "_", "_", "_", "_", "_", "_"], ["_", "_", "_", "_", "_", "_", "_", "_"], ["_", "_", "_", "_", "wp", "_", "_", "_"], ["_", "_", "_", "_", "_", "_", "_", "_"], ["wp", "wp", "wp", "wp", "_", "wp", "wp", "wp"], ["wr", "wh", "wb", "wq", "wk", "wb", "wh", "wr"]], "progress_state": {"description": "", "state": "INPROGRESS"}, "your_color": "white", "player_turn": "black"}}'
+		let message = JSON.parse(game_state_message_string);
+		let board = message.payload.board;
+
 		let context = this.canvas.getContext("2d");
 		if (context == null) {
 			throw new Error("Context was null");
 		}
 		context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-		// Loop over each checker square
+		// Loop over each checker square to draw the checker pattern
 		for (let row_index = 0; row_index < BOARD_ROWS; row_index++) {
 			for (let column_index = 0; column_index < BOARD_COLUMNS; column_index++) {
 				let checker_index = {x_offset: column_index, y_offset: row_index};
@@ -70,6 +100,7 @@ class Board{
 					context.fillStyle = DARK_CHECKER_PATTERN_COLOUR;
 				}
 				context.fillRect(checker_pixel_coord.x_offset, checker_pixel_coord.y_offset, NUM_WIDTH_PIXELS_PER_CHECKER_BOX, NUM_HEIGHT_PIXELS_PER_CHECKER_BOX);
+				context.font = "30px Arial"
 			}
 		}
 
@@ -86,6 +117,28 @@ class Board{
 			for (let i=0; i< possible_moves.length; i++) {
 				let move_option_pixel_coords = this.getPixelOffsetFromCheckerIndex(possible_moves[i]);
 				context.fillRect(move_option_pixel_coords.x_offset, move_option_pixel_coords.y_offset, NUM_WIDTH_PIXELS_PER_CHECKER_BOX, NUM_HEIGHT_PIXELS_PER_CHECKER_BOX);
+			}
+		}
+				
+		// Loop over each checker square to draw pieces
+		for (let row_index = 0; row_index < BOARD_ROWS; row_index++) {
+			for (let column_index = 0; column_index < BOARD_COLUMNS; column_index++) {
+				let checker_index = {x_offset: column_index, y_offset: row_index};
+				let checker_pixel_coord = this.getPixelOffsetFromCheckerIndex(checker_index);
+
+				let piece_string = board[row_index][column_index];
+				if (piece_string[0] == "w") {
+					context.fillStyle = "white"
+				} else {
+					context.fillStyle = "black"
+				}
+
+				if (piece_string != "_") {
+					context.textAlign = "center"
+					let unicode_string = this.convertStringToPiece(piece_string[0], piece_string[1]);
+					context.fillText(unicode_string, checker_pixel_coord.x_offset + NUM_WIDTH_PIXELS_PER_CHECKER_BOX/2,
+						checker_pixel_coord.y_offset + (2*NUM_HEIGHT_PIXELS_PER_CHECKER_BOX)/3);
+				}
 			}
 		}
 	}
